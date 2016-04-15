b4e10906c1c05ac7e99c9da0253d353dd2ec9ac5
Epicurean306 <benjaminforbesgriffith@gmail.com>
2016-04-14 20:48:03 -0400

    started working on JavaScript configuration for hangman functionality

--- assets/scripts/hangman.js
+++ assets/scripts/hangman.js
@@ -1,4 +1,34 @@
+/* HANGMAN */
 (function(window){
   'use strict';
+/* variables */
+var alphabet, wordBank, guess, guessesList, matchesList, matchesNumber, bodypartsRemain, mysteryWord, feedbackMessages;
+
+/* hangman controller */
+function configAssets() {
+  var alphabet =
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g',
+    'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  bodypartsRemain = 5;
+  wordBank =
+    ["spaghetti",
+    "poncho",
+    "wagon",
+    "stagecoach",
+    "colt"];
+  feedbackMessages = {
+    victory: "You're the fastest gunslinger in the West! Congratulations, victory is yours, and you've just escaped a hanging at high noon."
+    loss: "You lose pardner... Time for you to do the hangman's dance.";
+    alreadyGuessed: "You already guessed that letter sucker, try again.";
+    validateLetter: "Woah there cowboy: enter a letter from A-Z!";
+  };
+}
+/* initial guess state */
+guessesList = matchesList = '';
+matchesNumber = 0;
+
+/* word selection */
+
 
 })(window);

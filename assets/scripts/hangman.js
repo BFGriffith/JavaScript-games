d102de48a987fb095b4ace3571af56f178bec7e9
Epicurean306 <benjaminforbesgriffith@gmail.com>
2016-04-19 23:26:04 -0400

    starting functions in object refactor based on classwork examples, still working on styles

--- assets/scripts/hangman.js
+++ assets/scripts/hangman.js
@@ -1,34 +1,41 @@
 /* HANGMAN */
 (function(window){
   'use strict';
-/* variables */
-var alphabet, wordBank, guess, guessesList, matchesList, matchesNumber, bodypartsRemain, mysteryWord, feedbackMessages;
-
-/* hangman configuration */
-function configAssets() {
-  var alphabet =
-    ['a', 'b', 'c', 'd', 'e', 'f', 'g',
-    'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  bodypartsRemain = 5;
-  wordBank =
-    ["spaghetti",
+// var alphabet, wordBank, guess, guessesList, matchesList, matchesNumber, bodypartsRemain, mysteryWord, feedbackMessages;
+/*
+var alphabet =
+  ['a', 'b', 'c', 'd', 'e', 'f', 'g',
+  'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+  'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+*/
+/* functions in object refactor based on classwork examples */
+var hangman = {
+  bodypartsRemain: 10,
+  targetWordArray: [],
+  mysteryWord: "",
+  lettersGuessed: [],
+  matchesList: [],
+  winsCounter: 0,
+  wordBank:
+   ["spaghetti",
     "poncho",
     "wagon",
     "stagecoach",
-    "colt"];
+    "colt",
+    "cowboy",
+    "frontier",
+    "huckster"
+  ],
+  /* word selection... */
+
+  /*
   feedbackMessages = {
-    victory: "You're the fastest gunslinger in the West! Congratulations, victory is yours, and you've just escaped a hanging at high noon."
-    loss: "You lose pardner... Time for you to do the hangman's dance.";
-    alreadyGuessed: "You already guessed that letter sucker, try again.";
-    validateLetter: "Woah there cowboy: enter a letter from A-Z!";
+    victory: "You're the fastest gunslinger in the West! Congratulations, victory is yours, and you've just escaped a hanging at high noon.",
+    loss: "You lose pardner... Time for you to do the gallows dance.",
+    alreadyGuessed: "You already guessed that letter sucker, try again.",
+    validateLetter: "Woah there cowboy. You must enter a valid letter from A-Z!"
   };
-}
-/* initial guess state */
-guessesList = matchesList = '';
-matchesNumber = 0;
-
-/* word selection */
-
+  */
+}/*END-hangman-configuration_object*/
 
 })(window);
